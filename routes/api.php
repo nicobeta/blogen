432abd106921d10ff74e1826a408199db40082ae
Nico Beta <nico1485@gmail.com>
2017-09-21 14:33:00 -0300

    Use apiResource in router

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ Route::post('auth/signup', 'RegistrationController@store');
 Route::post('auth/login', 'SessionController@store');
 Route::get('auth/whoami', 'SessionController@show')->middleware('api.auth');
 
-Route::resource('posts', 'PostController')->except(['create', 'edit']);
-Route::resource('posts/{post}/comments', 'CommentController')->except(['create', 'edit']);
-Route::resource('categories', 'CategoryController')->except(['create', 'edit']);
+Route::apiResource('posts', 'PostController');
+Route::apiResource('posts/{post}/comments', 'CommentController');
+Route::apiResource('categories', 'CategoryController');
 Route::get('tags', 'TagController@index');
